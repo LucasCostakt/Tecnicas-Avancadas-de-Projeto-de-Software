449b0cbed6291b089f8bd21d4203dcbd5e1b3635
LucasCostakt <lucascosta.kt@gmail.com>
2020-09-06 15:07:09 -0300

    Alteração do código
    Feito a primeira parte da limpeza onde conciste na divisão das funções, uma vai imprimir no endpoint e outra que vai ler o arquivo

--- main.go
+++ main.go
@@ -8,8 +8,18 @@ import (
 )
 
 func handlerMeuCumprimento(w http.ResponseWriter, r *http.Request) {
-	letter, _ := ioutil.ReadFile("hellomundo.html")
-	fmt.Fprintf(w, string(letter))
+	nomeFile := "telainicio.html"
+	responseArquivo := lerArquivo(nomeFile)
+	fmt.Fprintf(w, string(responseArquivo))
+
+}
+
+func lerArquivo(nomeFile string) []byte {
+	conteudoFile, err := ioutil.ReadFile(nomeFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return conteudoFile
 }
 
 func main() {
